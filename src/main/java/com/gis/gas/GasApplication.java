37b4322277bd518192431d2e28477a1230f15f35
dongjun-yeong <djy1212@gmail.com>
2021-07-07 14:32:26 +0900

    Initial commit

---
+++ src/main/java/com/gis/gas/GasApplication.java
@@ -0,0 +1,13 @@
+package com.gis.gas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gis/gas/GasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gis.gas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
